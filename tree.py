d82a5f045b64f160a38dc2a139a61af65a37f9ba
nad-lspu <drantsolayao05@gmail.com>
2025-03-13 17:43:32 +0800

    CPE8 Lab

--- tree.py
+++ tree.py
@@ -4,8 +4,6 @@ class TreeNode:
         self.left = None
         self.right = None
 
-#In-0rder
-
 def inorder_traversal(root):
     if root:
         inorder_traversal(root.left)
@@ -18,22 +16,37 @@ def preorder_traversal(root):
         preorder_traversal(root.left)
         preorder_traversal(root.right)
 
-#Post Order
 def postorder_traversal(root):
     if root:
         postorder_traversal(root.left)
         postorder_traversal(root.right)
         print(root.value, end=" ")
 
+def swap_root(root):
+    if root:
+        root.left, root.right = root.right, root.left
+        swap_root(root.left)
+        swap_root(root.right)
+
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
-root.left.right = TreeNode(5)
 root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
 root.right.left = TreeNode(6)
 root.right.right = TreeNode(7)
 
-print("Inorder traversal")
+print("\nPre-order traversal")
+preorder_traversal(root)
+print("\nInorder traversal")
+inorder_traversal(root)
+print("\nPost-order traversal")
+postorder_traversal(root)
+
+print("\n\nSwapping...")
+swap_root(root)
+
+print("\nInorder traversal")
 inorder_traversal(root)
 print("\nPre-order traversal")
 preorder_traversal(root)
